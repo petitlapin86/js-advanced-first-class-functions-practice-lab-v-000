590f48e46e844eea49102e5971404848c93ce25c
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-13 17:57:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// Code your solution in this file!
 const logDriverNames = function (drivers) {
   drivers.forEach(function (driver) {
     console.log(driver.name);
   });
 };
-
-const logDriversByHometown = function (drivers, hometown) {
-  drivers.forEach(function (localDriver) {
-    if (localDriver.hometown === hometown) {
-      console.log(localDriver.name);
-    }
-  });
-};
-
 
 const logDriversByHometown = function (drivers, hometown) {
   drivers.forEach(function (driver) {
